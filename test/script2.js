a3bf55dfe7d25c0072e58899e320123c4fd3d7b2
João Victor Ornelas <84112302+joaox777@users.noreply.github.com>
2023-08-24 21:18:38 -0300

    addinfs

--- test/script2.js
+++
@@ -1,6 +0,0 @@
-const chk = document.getElementById('chk');
-
-chk.addEventListener('change', () => {
-  const container = document.getElementById('container-01');
-  container.classList.toggle('dark');
-});
